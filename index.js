68f107ab385241153d2cec43ff766d7f08e12265
Polycarp Ace <shakyxavier@gmail.com>
2019-06-12 17:56:01 +0300

    DB action

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require("express");
+const mysql = require("mysql");
 const app = express();
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.json({message:"Hello World!"}));
+app.get("/api/customer", (req, res)=> {
+    pool.query("SELECT customer_id, first_name, last_name, date_registered FROM customer", (error, rows)=> {
+        if(error) {
+            return res.status(500).json({error});
+
+        }
+        res.json(rows);
+    } );
+
+});
 
 app.listen(9000, () => console.log("App listening on port 9000"));
 
