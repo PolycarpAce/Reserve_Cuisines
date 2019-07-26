ec674cf920fc2cb0909c4da8fee09e2a9de6146e
Polycarp Ace <shakyxavier@gmail.com>
2019-07-26 10:10:51 +0300

    chg

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {Router, Route, Switch, Redirect} from "react-router-dom";
+import {createBrowserHistory} from "history";
+import App from "./App.js";
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const hst = createBrowserHistory();
+
+ReactDOM.render(
+<Router history={hst} basename={"/"}> 
+    <Switch>
+        <Route path ="/" component={App} exact/>
+        
+    </Switch> 
+    </Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
